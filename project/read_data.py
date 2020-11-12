1ee56e0b71cfdc220cb59e48a55fa1bf0c095bd0
phani-1995 <phanindrasrys@gmail.com>
2020-11-12 09:52:29 +0530

    reading data

--- project/read_data.py
+++ project/read_data.py
@@ -23,26 +23,64 @@ schema = StructType([StructField("cc_num", StringType(), True),
                      StructField("merch_lat", StringType(), True),
                      StructField("merch_long", StringType(), True),
                      StructField("is_fraud", StringType(), True)])
+
+schema1 = StructType([StructField("cc_num", StringType(), True),
+                     StructField("first", StringType(), True),
+                     StructField("last", StringType(), True),
+                     StructField("gender", StringType(), True),
+                     StructField("street", StringType(), True),
+                     StructField("city", StringType(), True),
+                     StructField("state", StringType(), True),
+                     StructField("zip", StringType(), True),
+                     StructField("lat", StringType(), True),
+                     StructField("long", StringType(), True),
+                     StructField("job", StringType(), True),
+                     StructField("dob", StringType(), True)])
 def read_data():
     #Read data from HDFS
-    input_path = "hdfs://localhost:54310/user/data/transcations/*.json"
+    input_path = "hdfs://localhost:54310/user/data/transcations1/*.json"
     trans = spark\
         .read\
         .schema(schema)\
         .json(input_path)
     return trans
+
+def read_cust():
+    # Read data from HDFS
+    input_path = "hdfs://localhost:54310/user/data/customers1/*.json"
+    cust = spark \
+        .read \
+        .schema(schema1) \
+        .json(input_path)
+    return cust
 df = read_data()
 print(df.show())
+print(df.count())
+
 
 def write_data():
     #Writing data to cassandra
     df.write \
         .format("org.apache.spark.sql.cassandra") \
         .mode('append') \
-        .options(table="transactions", keyspace="transactions_db") \
+        .options(table="transactions1", keyspace="transactions_db") \
         .save()
+
+df1 = read_cust()
+print(df1.show())
+print(df1.count())
+def write_cust():
+    #Writing data to cassandra
+    df1.write \
+        .format("org.apache.spark.sql.cassandra") \
+        .mode('append') \
+        .options(table="customers", keyspace="transactions_db")  \
+        .save()
+
 
 
 if __name__=="__main__":
     read_data()
     write_data()
+    read_cust()
+    # write_cust()
