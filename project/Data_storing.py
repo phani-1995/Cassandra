4a335f459b8f1e10ee508370fb66dd932972b4c9
phani-1995 <phanindrasrys@gmail.com>
2020-11-12 10:01:25 +0530

    Storing data in hdfs

--- project/Data_storing.py
+++ project/Data_storing.py
@@ -10,11 +10,22 @@ sc = spark.sparkContext
 def write_data():
     df = spark.read.option('header', True).csv('transactions.csv')
     # Dropping null values
-    df = df.dropna()
+    df = df.na.fill(0)
     print(df.printSchema())
     print(df.show())
     #stroing data in hdfs
-    df.write.json("hdfs://localhost:54310/user/data/transcations", mode='append')
+    df.write.json("hdfs://localhost:54310/user/data/transcations1", mode='append')
+
+def write_cust():
+    df1 = spark.read.option('header', True).csv('customer.csv')
+    # Dropping null values
+    df1 = df1.na.fill(0)
+    print(df1.printSchema())
+    print(df1.show())
+    # stroing data in hdfs
+    df1.write.json("hdfs://localhost:54310/user/data/customers1", mode='append')
+
 
 if __name__=="__main__":
     write_data()
+    write_cust()
