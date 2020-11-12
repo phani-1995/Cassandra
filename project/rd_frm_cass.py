5eecf467a4236be92a968ad19884b18c2ba864e7
phani-1995 <phanindrasrys@gmail.com>
2020-11-12 09:53:58 +0530

    Reading data from cassandra

--- project/rd_frm_cass.py
+++ project/rd_frm_cass.py
@@ -5,6 +5,12 @@ os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassand
 from pyspark import SparkContext
 sc = SparkContext("local", "students app")
 
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession.builder.appName("Classification with Spark").getOrCreate()
+
+
 from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
 
@@ -15,8 +21,22 @@ def load_and_get_table_df(keys_space_name, table_name):
         .load()
     return table_df
 
-trans = load_and_get_table_df("transactions_db", "transactions")
+trans = load_and_get_table_df("transactions_db", "transactions1")
 print(trans.show())
+print(trans.count())
+def load_and_get_table_df1(keys_space_name, table_name):
+    table_df1 = sqlContext.read\
+        .format("org.apache.spark.sql.cassandra")\
+        .options(table=table_name, keyspace=keys_space_name)\
+        .load()
+    return table_df1
+
+trans1 = load_and_get_table_df1("transactions_db", "customers")
+print(trans1.show())
+
+
+print(trans1.count())
+
 
 
 
